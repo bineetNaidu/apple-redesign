64ec00a6481b7c9962e2252c94820144f3140bb0
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-01-21 17:25:45 +0530

    🪨 add Poppins font and extent tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
