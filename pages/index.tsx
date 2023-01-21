c14e6c7af85d8110424558a3e2b02312a2800021
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-01-21 14:21:15 +0530

    🎉 add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Apple redesign</h1>
+        <h1 className="text-3xl">Apple redesign</h1>
       </main>
     </>
   );
