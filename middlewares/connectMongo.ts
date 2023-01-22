baa6bcdd12a7f2512e46146ca8aca7e0623885c3
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-01-22 20:03:20 +0530

    🚀 add mongoose connect config

---
+++ middlewares/connectMongo.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type ConnectDb = (
+  handler: (req: NextApiRequest, res: NextApiResponse) => void
+) => (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
+
+export const connectDB: ConnectDb = (handler) => async (req, res) => {
+  if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return handler(req, res);
+  }
+  // Use new db connection
+  await mongoose.connect(process.env.MONGODB_URI!);
+
+  return handler(req, res);
+};
