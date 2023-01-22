4cc481298fa7fb2da41b28e4328d7cbe26d9f493
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-01-22 19:49:57 +0530

    🪨 update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
 import { Header } from '@/components/Header';
 import { Landing } from '@/components/Landing';
+import { Tab } from '@headlessui/react';
+import { type GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const categories = await fetchCategories();
+
+  return {
+    props: {},
+  };
+};
 
 export default function Home() {
+  const [categories, setCategories] = useState<Category[]>([
+    {
+      _id: 'iphone',
+      title: 'Iphones',
+      slug: 'as',
+    },
+    {
+      _id: 'mac',
+      title: 'Macs',
+      slug: 'sda',
+    },
+  ]);
+  const showProducts: any = (n: number) => {};
   return (
     <>
       <Head>
@@ -16,6 +40,39 @@ export default function Home() {
       <main className="relative h-[200vh] bg-[#e7ecee]">
         <Landing />
       </main>
+      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1b1b1b]">
+        <div className="space-y-8 py-16 ">
+          <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-2xl">
+            New Promos
+          </h1>
+
+          <Tab.Group>
+            <Tab.List className="flex justify-center">
+              {categories.map((category) => (
+                <Tab
+                  key={category._id}
+                  id={category._id}
+                  className={({ selected }) =>
+                    `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                      selected
+                        ? 'borderGradient bg-[#35383C] text-white'
+                        : 'border-b-2 border-[#35383C] text-[#747474]'
+                    }`
+                  }
+                >
+                  {category.title}
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+              <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </section>
     </>
   );
 }
