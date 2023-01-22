33ba1bb26159925f33c93435e2bf327a628ac28f
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-01-22 20:35:07 +0530

    ✨ add getCategories api route

--- pages/api/getCategories.ts
+++ pages/api/getCategories.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { connectDB } from '@/middlewares/connectMongo';
+import { CategoryModel, Category } from '@/models/category';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   categories: Category[];
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  // const categories = await sanityClient.fetch(query);
+async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const categories = await CategoryModel.find({});
+
+  res.status(200).json({ categories });
 }
+
+export default connectDB(handler);
